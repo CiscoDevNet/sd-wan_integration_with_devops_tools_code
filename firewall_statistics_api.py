08c765bc5e3881bb9d8492c6b8f59783479c018b
suchandanreddy <saisuchandan@gmail.com>
2020-06-22 08:49:25 -0700

    update authentication for 19.2

--- firewall_statistics_api.py
+++ firewall_statistics_api.py
@@ -5,137 +5,197 @@ import json
 import os
 import pprint
 import time
-
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
-vmanage_host = os.environ.get("vmanage_host")
-vmanage_port = os.environ.get("vmanage_port")
-username = os.environ.get("username")
-password = os.environ.get("password")
-
-
-if vmanage_host is None or vmanage_port is None or username is None or password is None:
-    print("For Windows Workstation, vManage details must be set via environment variables using below commands")
-    print("set vmanage_host=198.18.1.10")
-    print("set vmanage_port=443")
-    print("set username=admin")
-    print("set password=admin")
-    print("For MAC OSX Workstation, vManage details must be set via environment variables using below commands")
-    print("export vmanage_host=198.18.1.10")
-    print("export vmanage_port=443")
-    print("export username=admin")
-    print("export password=admin")
-    exit()
+import yaml
+from logging.handlers import TimedRotatingFileHandler
 
 requests.packages.urllib3.disable_warnings()
 
-class rest_api_lib:
-    def __init__(self, vmanage_host,vmanage_port, username, password):
-        self.vmanage_host = vmanage_host
-        self.vmanage_port = vmanage_port
-        self.session = {}
-        self.login(self.vmanage_host, username, password)
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-    def login(self, vmanage_host, username, password):
+def get_logger(logfile, level):
+    '''
+    Create a logger
+    '''
+    if logfile is not None:
+
+        '''
+        Create the log directory if it doesn't exist
+        '''
+
+        fldr = os.path.dirname(logfile)
+        if not os.path.exists(fldr):
+            os.makedirs(fldr)
+
+        logger = logging.getLogger()
+        logger.setLevel(level)
+ 
+        log_format = '%(asctime)s | %(levelname)-8s | %(funcName)-20s | %(lineno)-3d | %(message)s'
+        formatter = logging.Formatter(log_format)
+ 
+        file_handler = TimedRotatingFileHandler(logfile, when='midnight', backupCount=7)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(level)
+        logger.addHandler(file_handler)
+
+        return logger
+
+    return None
+
+
+class Authentication:
+
+    @staticmethod
+    def get_jsessionid(vmanage_host, vmanage_port, username, password):
+        api = "/j_security_check"
+        base_url = "https://%s:%s"%(vmanage_host, vmanage_port)
+        url = base_url + api
+        payload = {'j_username' : username, 'j_password' : password}
         
-        """Login to vmanage"""
+        response = requests.post(url=url, data=payload, verify=False)
+        try:
+            cookies = response.headers["Set-Cookie"]
+            jsessionid = cookies.split(";")
+            return(jsessionid[0])
+        except:
+            if logger is not None:
+                logger.error("No valid JSESSION ID returned\n")
+            exit()
+       
+    @staticmethod
+    def get_token(vmanage_host, vmanage_port, jsessionid):
+        headers = {'Cookie': jsessionid}
+        base_url = "https://%s:%s"%(vmanage_host, vmanage_port)
+        api = "/dataservice/client/token"
+        url = base_url + api      
+        response = requests.get(url=url, headers=headers, verify=False)
+        if response.status_code == 200:
+            return(response.text)
+        else:
+            return None
 
-        base_url = 'https://%s:%s/'%(self.vmanage_host, self.vmanage_port)
+if __name__ == '__main__':
 
-        login_action = '/j_security_check'
+    try:
 
-        #Format data for loginForm
-        login_data = {'j_username' : username, 'j_password' : password}
+        log_level = logging.DEBUG
+        logger = get_logger("log/firewall_stats.txt", log_level)
 
-        #Url for posting login data
-        login_url = base_url + login_action
-        url = base_url + login_url
+        if logger is not None:
+            logger.info("Loading vManage login details from YAML\n")
+        with open("vmanage_login.yaml") as f:
+            config = yaml.safe_load(f.read())
 
-        sess = requests.session()
+        vmanage_host = config["vmanage_host"]
+        vmanage_port = config["vmanage_port"]
+        username = config["vmanage_username"]
+        password = config["vmanage_password"]
 
-        #If the vmanage has a certificate signed by a trusted authority change verify to True
+        Auth = Authentication()
+        jsessionid = Auth.get_jsessionid(vmanage_host,vmanage_port,username,password)
+        token = Auth.get_token(vmanage_host,vmanage_port,jsessionid)
 
-        login_response = sess.post(url=login_url, data=login_data, verify=False)
+        if token is not None:
+            headers = {'Content-Type': "application/json",'Cookie': jsessionid, 'X-XSRF-TOKEN': token}
+        else:
+            headers = {'Content-Type': "application/json",'Cookie': jsessionid}
 
-    
-        if b'<html>' in login_response.content:
-            print ("Login Failed")
-            sys.exit(0)
+        base_url = "https://%s:%s/dataservice"%(vmanage_host,vmanage_port)
 
-        self.session[vmanage_host] = sess
+        payload = {
+                    "query": {
+                        "condition": "AND",
+                        "rules": [
+                        {
+                            "value": [
+                            "24"
+                            ],
+                            "field": "entry_time",
+                            "type": "date",
+                            "operator": "last_n_hours"
+                        },
+                        {
+                            "value": [
+                            "total"
+                            ],
+                            "field": "type",
+                            "type": "string",
+                            "operator": "in"
+                        }
+                        ]
+                    },
+                    "aggregation": {
+                        "metrics": [
+                                        {
+                                            "property": "fw_total_insp_count",
+                                            "type": "sum",
+                                            "order": "desc"
+                                        }
+                                    ],
+                        "histogram": {
+                                        "property": "entry_time",
+                                        "type": "minute",
+                                        "interval": 30,
+                                        "order": "asc"
+                                     }
+                    }
+                    }
 
-    def get_request(self, mount_point):
-        """GET request"""
-        url = "https://%s:%s/dataservice/%s"%(self.vmanage_host, self.vmanage_port, mount_point)
-        print(url)
-      
-        response = self.session[self.vmanage_host].get(url, verify=False)
-        
-        return response
+        api_url = "/statistics/fwall/aggregation"
 
-    def post_request(self, mount_point, payload, headers={'Content-type': 'application/json', 'Accept': 'application/json'}):
-        """POST request"""
-        url = "https://%s:%s/dataservice/%s"%(self.vmanage_host, self.vmanage_port, mount_point)
-        #print(url)
-        payload = json.dumps(payload)
-        #print (payload)
+        url = base_url + api_url
 
-        response = self.session[self.vmanage_host].post(url=url, data=payload, headers=headers, verify=False)
-        #print(response.text)
-        #exit()
-        #data = response
-        return response
+        response = requests.post(url=url, headers=headers, data=json.dumps(payload), verify=False)
+
+        if response.status_code == 200:
+            items = response.json()['data']
+        else:
+            print("\nFailed to retrieve Firewall Inspect Session statistics")
+            if logger is not None:
+                logger.error("\nFailed to retrieve Firewall Inspect Session statistics")
+            exit()
 
 
+        # login credentials for InfluxDB
+
+        USER = 'root'
+        PASSWORD = 'root'
+        DBNAME = 'firewall_inspect'
 
 
-vmanage_session = rest_api_lib(vmanage_host, vmanage_port, username, password)
+        host='localhost'
+        port=8086
 
-payload = {"aggregation":{"metrics":[{"property":"fw_total_insp_count","type":"sum","order":"desc"}],"histogram":{"property":"entry_time","type":"minute","interval":30,"order":"asc"}},"query":{"condition":"AND","rules":[{"value":["24"],"field":"entry_time","type":"date","operator":"last_n_hours"},{"value":["total"],"field":"type","type":"string","operator":"in"}]}}
+        series = []
+        total_records = 0
 
-response = vmanage_session.post_request("statistics/fwall/aggregation",payload)
+        json_body = {}
 
-items = response.json()['data']
+        # loop over the API response variable items and create records to be stored in InfluxDB
 
+        for i in items:
+            json_body = {
+                        "measurement": "firewall_inspect_count",
+                        "tags": {
+                                    "host": "wan_edge",
+                                },
+                        "time": time.strftime('%m/%d/%Y %H:%M:%S',  time.gmtime(i['entry_time']/1000.)),
+                        "fields": {
+                                    "value": float(i['fw_total_insp_count'])
+                        }
+                        }
+            series.append(json_body)
+            total_records = total_records+1
 
+        client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
 
-#login credentials for InfluxDB
+        print("Create a retention policy")
+        retention_policy = 'retention_policy_1'
+        client.create_retention_policy(retention_policy, '10d', 3, default=True)
 
-USER = 'root'
-PASSWORD = 'root'
-DBNAME = 'firewall_inspect'
+        print("Write points #: {0}".format(total_records))
+        client.write_points(series, retention_policy=retention_policy)
 
+        time.sleep(2)
 
-host='localhost'
-port=8086
-
-series = []
-total_records = 0
-
-json_body = {}
-
-#loop over the API response variable items and create records to be stored in InfluxDB
-
-for i in items:
-    json_body = { "measurement": "firewall_inspect_count",
-                  "tags": {
-                            "host": "wan_edge",
-                          },
-                 "time": time.strftime('%m/%d/%Y %H:%M:%S',  time.gmtime(i['entry_time']/1000.)),
-                 "fields": {
-                            "value": float(i['fw_total_insp_count'])
-                }
-                }
-    series.append(json_body)
-    total_records = total_records+1
-
-client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
-
-print("Create a retention policy")
-retention_policy = 'retention_policy_1'
-client.create_retention_policy(retention_policy, '10d', 3, default=True)
-
-print("Write points #: {0}".format(total_records))
-client.write_points(series, retention_policy=retention_policy)
-
-time.sleep(2)
+    except Exception as e:
+        print('Exception line number: {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
